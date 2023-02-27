ed613c8266fb1d9825bd680208e59503a43b6734
ykono <yfukyd@gmail.com>
2023-02-28 00:01:26 +0900

    dbスキーマを変更

--- mysql/init/00_create.sql
+++ mysql/init/00_create.sql
@@ -1,10 +1,23 @@
-CREATE DATABASE IF NOT EXISTS rush_price;
-USE rush_price;
-DROP TABLE IF EXISTS cards;
-CREATE TABLE IF NOT EXISTS cards(
+CREATE DATABASE IF NOT EXISTS ptcwave;
+USE ptcwave;
+DROP TABLE IF EXISTS rush_cards;
+CREATE TABLE IF NOT EXISTS rush_cards(
   name VARCHAR(50) NOT NULL,
   version VARCHAR(100) NOT NULL,
   rarity VARCHAR(10) NOT NULL,
+  type VARCHAR(10) NOT NULL,
+  price decimal NOT NULL,
+  status VARCHAR(255) NOT NULL DEFAULT '-',
+  fetch_date datetime NOT NULL DEFAULT current_timestamp,
+  created_at datetime NOT NULL DEFAULT current_timestamp,
+  PRIMARY KEY(name, version, rarity, fetch_date)
+);
+
+DROP TABLE IF EXISTS hareruya_cards;
+CREATE TABLE IF NOT EXISTS hareruya_cards(
+  name VARCHAR(50) NOT NULL,
+  version VARCHAR(100) NOT NULL,
+  rarity VARCHAR(50) NOT NULL,
   type VARCHAR(10) NOT NULL,
   price decimal NOT NULL,
   status VARCHAR(255) NOT NULL DEFAULT '-',
