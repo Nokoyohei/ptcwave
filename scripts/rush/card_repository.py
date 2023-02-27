008d11c48fd9546b84bab437da31af278d08e91f
ykono <yfukyd@gmail.com>
2023-02-27 23:55:41 +0900

    ショップごとにディレクトリを分割

---
+++ scripts/rush/card_repository.py
@@ -0,0 +1,44 @@
+import mysql.connector
+from card import Card
+from datetime import datetime
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+conn = mysql.connector.connect(
+  user='root',
+  host='localhost',
+  password=os.getenv('MYSQL_PASSWORD'),
+  database='ptcwave'
+)
+
+class CardRepository:
+  def __init__(self):
+    pass
+
+  def create_cards(self, cards, date=None):
+    fetch_date = date
+    if fetch_date is None:
+      fetch_date = datetime.now().strftime('%Y-%m-%d')
+    sql = '''INSERT INTO rush_cards 
+      (
+        name, 
+        status, 
+        rarity, 
+        version, 
+        type, 
+        price, 
+        fetch_date
+      ) 
+    VALUES 
+      (%s, %s, %s, %s, %s, %s, %s)
+    ON DUPLICATE KEY UPDATE price=VALUES(price)
+    '''
+
+    cards = [card.to_sql_object() + [fetch_date] for card in cards
+      if card.to_sql_object() is not None and card.name != 'ウールー']
+
+    with conn.cursor() as cursor:
+      cursor.executemany(sql, cards)
+      conn.commit()
+
